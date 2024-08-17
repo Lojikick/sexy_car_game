97f1b1c97f354d15eabb1999f7ed82bc92e859f2
Avik Samanta <97483380+Lojikick@users.noreply.github.com>
2024-08-16 20:42:01 -0700

    Added some minor changes

--- modules/car.gd
+++ modules/car.gd
@@ -25,12 +25,12 @@ func _input(event):
 		#pivot.rotation.x = clamp(pivot.rotation.x, deg_to_rad(-90), deg_to_rad(45))
 func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	#if not is_on_floor():
+		#velocity.y -= gravity * delta
+#
+	## Handle jump.
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
 	
 	#Handle player exiting of the game:
 	if Input.is_action_just_pressed("escape"):
