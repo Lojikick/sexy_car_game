568feedc5d6f10e52a762094976b3f7882f231c7
Avik Samanta <97483380+Lojikick@users.noreply.github.com>
2024-08-20 06:31:01 -0700

    Finally got this fumass game connected

--- menu/menu.gd
+++ menu/menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/dummy_level.tscn")
+	get_tree().change_scene_to_file("res://menu/sexy_controls_menu.tscn")
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
